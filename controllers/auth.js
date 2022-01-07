d00dc649adb6392a6f4f4d171567cc02fad0e5c9
revathi <revathi040798@gmail.com>
2022-01-07 17:43:57 +0530

    first commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -78,7 +78,7 @@ exports.forgotPassword = async (req, res, next) => {
     await user.save();
 
     // Create reset url to email to provided email
-    const resetUrl = `http://localhost:3000/passwordreset/${resetToken}`;
+    const resetUrl = `https://jwtauthen.herokuapp.com/passwordreset/${resetToken}`;
 
     // HTML Message
     const message = `
